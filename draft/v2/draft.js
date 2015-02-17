7e3bd6c289956d4f2f90aefd023b2de6e1dcbb76
CamiloMM <herzschaltung@yahoo.com.ar>
2015-02-17 03:13:10 -0200

    Second draft for the client-side implementation updated for backend changes.

--- draft/v2/draft.js
+++ draft/v2/draft.js
@@ -12,12 +12,15 @@ $(function() {
             playback.started = json.started;
             playback.paused = json.paused;
             if (json.track) {
+                var re = /^(.*?)(\.([^.]*))?$/;
                 var t = json.track, p = playback.track = {
                     length   : parseFloat(t.seconds),
                     position : parseFloat(t.seek),
                     artist   : t.artist ? t.artist[0] : '',
                     album    : t.album  ?  t.album[0] : '',
                     title    : t.title  ?  t.title[0] : '',
+                    filename : t.filename_ext ? t.filename_ext[0].replace(re, '$1') : '',
+                    ext      : t.filename_ext ? t.filename_ext[0].replace(re, '$3') : '',
                 };
                 playback.percent = p.length ? +(p.position / p.length * 100).toFixed(2) : 0;
             } else {
@@ -35,6 +38,5 @@ $(function() {
         if (err) return console.error(err);
         if (!playback) { console.log('open'); return $('#info').text('Waiting...'); }
         $('#info').text(JSON.stringify(playback));
-        console.log(playback);
     });
 });
